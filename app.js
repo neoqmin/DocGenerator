c99bc09abbe95f3b625189f4911a12d09216e501
Kevin Hwang <neoqmin@gmail.com>
2015-07-27 14:01:54 +0900

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 var express = require('express');
 
+var app = express();
+
 app.get('/', function (req, res) {
     res.writeHead(200, {'Content-Type': 'text/html'});
     res.write("Hello World!!");
